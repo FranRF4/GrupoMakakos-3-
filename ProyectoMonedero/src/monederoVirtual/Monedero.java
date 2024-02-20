e86d48be0ce1ed0c8f7cf37e59df716c53b824a6
Humanito04 <pablohumanesgandul04@gmail.com>
2024-02-20 14:39:30 +0100

    Creacion de atributos, constructores, getters y setters

--- ProyectoMonedero/src/monederoVirtual/Monedero.java
+++ ProyectoMonedero/src/monederoVirtual/Monedero.java
@@ -1,5 +1,32 @@
 package monederoVirtual;
 
 public class Monedero {
-
+	// Atributos
+		private String id;
+		private double saldo;
+		
+	// Constructor
+		public Monedero(String id, double saldo) {
+			this.id = id;
+			this.saldo = saldo;
+		}
+		public Monedero(String id) {
+			this.id = id;
+			this.saldo = 0.0;
+		}
+		
+		// Getters y Setters
+		public String getId() {
+			return this.id;
+		}
+		public void setId(String id) {
+			this.id = id;
+		}
+		public double getSaldo() {
+			return this.saldo;
+		}
+		public void setSaldo(double saldo) {
+			this.saldo = saldo;
+		}
+		
 }
