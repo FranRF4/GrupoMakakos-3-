5d4b7ba33e68cab57c130b35a4e5b0af8107307a
FranRF4 <fran.rf04@gmail.com>
2024-02-21 18:40:34 +0100

    Método transferencia y gastoDinero

--- ProyectoMonedero/src/monederoVirtual/Monedero.java
+++ ProyectoMonedero/src/monederoVirtual/Monedero.java
@@ -15,18 +15,27 @@ public class Monedero {
 		private String id;
 		private double saldo;
 		private double cantidadIngreso;
+		private double cantidadGastada;
 		private String caracteres = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+		private double cantidadTransferida;
 		
 	// Constructor
 		public Monedero( double saldo) {
 			this.id = creacionId();
 			this.saldo = saldo;
+			this.cantidadIngreso = 0.0;
+			this.cantidadGastada = 0.0;
+			this.cantidadTransferida = 0.0;
 		}
 		public Monedero() {
 			this.id = creacionId();
 			this.saldo = 0.0;
+			this.cantidadIngreso = 0.0;
+			this.cantidadGastada = 0.0;
+			this.cantidadTransferida = 0.0;
 		}
 		
+
 		
 		// Getters y Setters
 		public String getId() {
@@ -43,11 +52,12 @@ public class Monedero {
 			this.saldo = saldo;
 		}
 		
+		
 		//Métodos
 		public double ingresar(double cantidadIngreso) {
-			System.out.println("¿Qué cantidad de dinero quieres ingresar a tu saldo");
-			if(cantidadIngreso <= 0) {
-				System.out.println("No puedes ingresar esa cantidad. Debes ingresar una cantidad mayor de 0");
+			System.out.println("¿Qué cantidad de dinero quieres ingresar a tu saldo?");
+			if(cantidadIngreso <= 0.0) {
+				System.out.println("No puedes ingresar esa cantidad. Debes ingresar una cantidad mayor de 0.0€");
 			}else {
 				System.out.println("Perfecto. Cantidad ingresada correctamente");
 				this.saldo = saldo + cantidadIngreso;
@@ -56,6 +66,28 @@ public class Monedero {
 		}
 		
 		
+		public double transferencia(double cantidadTransferida) {
+			System.out.println("¿Qué cantidad de dinero quieres transferir a otra cuenta?" );
+			if(cantidadTransferida <= 0.0 ) {
+				System.out.println("Error. El dinero ha transferir tiene que ser mayor que 0.0€");
+			}else {
+				System.out.println("Perfecto. La transferencia se ha efectuado correctamente");
+				this.saldo = saldo - cantidadTransferida;
+			}
+			return saldo;
+		}
+		
+		
+		public double gastoDinero (double cantidadGastada) {
+			System.out.println("Has gastado" + cantidadGastada);
+			if(cantidadGastada <= 0.0) {
+				System.out.println("Error. No has gastado nada este mes ");
+			}else {
+				System.out.println("Perfecto. Cantidad pagada correctamente");
+				this.saldo = saldo - cantidadGastada;
+			}
+			return saldo;
+		}
 			
 		
 		public String creacionId() {
