5299f7235ebcdc1f7a4f26255bc32952a354bf95
Humanito04 <pablohumanesgandul04@gmail.com>
2024-02-21 17:25:42 +0100

    Comienzo del primer método

--- ProyectoMonedero/src/monederoVirtual/Monedero.java
+++ ProyectoMonedero/src/monederoVirtual/Monedero.java
@@ -1,9 +1,17 @@
 package monederoVirtual;
 
 public class Monedero {
+	/* Implementa la clase monedero virtual: 
+		 Cada monedero tiene un ID de 12 dígitos. 
+		 Para simplificar, el ID se genera de forma aleatoria cuando se crea un monedero nuevo. 
+		 El monedero se puede crear con un saldo inicial; en caso de no especificar saldo, se pondrá a cero inicialmente. 
+		 En un monedero se pueden hacer ingresos y gastos. 
+		 También es posible hacer una transferencia entre un monedero y otro. 
+		 Se permite el saldo negativo.*/
 	// Atributos
 		private String id;
 		private double saldo;
+		private double cantidadIngreso;
 		
 	// Constructor
 		public Monedero(String id, double saldo) {
@@ -26,7 +34,13 @@ public class Monedero {
 			return this.saldo;
 		}
 		public void setSaldo(double saldo) {
+
 			this.saldo = saldo;
 		}
 		
+		//Métodos
+		public void ingresar(double cantidadIngreso) {
+			this.saldo = saldo + cantidadIngreso;
+		}
+	
 }
